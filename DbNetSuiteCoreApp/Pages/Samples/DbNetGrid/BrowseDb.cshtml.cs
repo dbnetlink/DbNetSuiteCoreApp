4a7a97b3ce1778978467880717a6cc48afb6a4bb
Robin Coode <robin@dbnetlink.co.uk>
2023-09-17 14:28:40 +0100

    Updated to use the latest version

---
+++ DbNetSuiteCoreApp/Pages/Samples/DbNetGrid/BrowseDb.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using DbNetSuiteCoreApp.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace DbNetSuiteCoreApp.Pages.Samples.DbNetGrid
+{
+    public class BrowseDbModel : SampleModel
+    {
+        public BrowseDbModel(IConfiguration configuration, IWebHostEnvironment env) : base(configuration, env)
+        {
+        }
+    }
+}
---
+++ DbNetSuiteCoreApp/Pages/Samples/DbNetGrid/Localisation.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using DbNetSuiteCoreApp.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace DbNetSuiteCoreApp.Pages.Samples.DbNetGrid
+{
+    public class LocalizationModel : SampleModel
+    {
+        public LocalizationModel(IConfiguration configuration, IWebHostEnvironment env) : base(configuration, env)
+        {
+        }
+    }
+}
